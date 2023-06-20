9de7bcf9833db4be8bee185a1ddb000475a55c00
borodkoleg <borodkoleg@gmail.com>
2023-06-20 19:54:50 +0300

    meta tags and title on page

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,17 @@
+import Head from "next/head";
 import A from "../components/A";
 
 const Index = () => {
   return (
     <>
+      <Head>
+        <title>Main page</title>
+        <meta keywords="next start, meta tag"></meta>
+      </Head>
       <div className="navBar">
-        <A href="/" text="Main"/>
+        <A href="/" text="Main" />
         <br />
-        <A href="/users" text="Users"/>
+        <A href="/users" text="Users" />
       </div>
       <h1>Main page</h1>
     </>
