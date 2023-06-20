e0da468b1266d187c04bc18670802d1a9f04b7c2
borodkoleg <borodkoleg@gmail.com>
2023-06-20 19:46:59 +0300

    server render for [id] pages

--- pages/users/[id].js
+++ pages/users/[id].js
@@ -1,11 +1,22 @@
 import { useRouter } from "next/router"
 
-export default function () {
-  const {query} = useRouter();
+export default function ({ user }) {
+  const { query } = useRouter();
 
   return (
     <>
-      user {query.id}
+      page id - {query.id}
+      <br /><br />
+      {user.title}
     </>
   )
+}
+
+export async function getServerSideProps({ params }) {
+  const response = await fetch(`https://my-json-server.typicode.com/typicode/demo/posts/${params.id}`);
+  const user = await response.json();
+  console.log(user);
+  return {
+    props: { user },
+  }
 }
